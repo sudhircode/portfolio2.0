94272c8aeb424edf3a15e4d976a100ab46b14927
Sudhir-kumar01 <mauryasudhir635@gmail.com>
2024-07-07 19:32:04 +0530

    Updates

--- src/Components/Begin_anim.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Begin_anim() {
-  return (
-    <div style={{textAlign:'center',fontSize:'2em',color:'black'}}>Begin_anim</div>
-  )
-}
--- src/Components/Journey.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Journey() {
-  return (
-    <div id='journey'>Journey</div>
-  )
-}
